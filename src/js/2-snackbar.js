b103c12e86f3019fabf6c39ef34854b747ce94ce
Soyakam <108721019+Soyakam@users.noreply.github.com>
2024-03-17 20:09:45 +0100

    10

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,13 +1,7 @@
-
 import iziToast from "izitoast";
-
 import "izitoast/dist/css/iziToast.min.css";
 
 const formOfDalay = document.querySelector(".form");
-
-
-
-
 
 formOfDalay.addEventListener('submit', event => {
   event.preventDefault();
@@ -16,36 +10,28 @@ formOfDalay.addEventListener('submit', event => {
 
   const promise = new Promise((resolve, reject) => {
     setTimeout(() => {
-
       console.log(timer, radio );
       if (radio ==="fulfilled") {
-        resolve(
-  
-          );
+        resolve();
       } else {
-        reject(
-    
-        );
+        reject();
       }
     }, timer);
   });
 
-
-
-promise
-  .then(value => {
-    iziToast.success({  
-      color: 'green',
-      position: "topRight",
-      message: `✅ Fulfilled promise in ${timer}ms`
-  })
-  })
-  .catch(error => {
-    iziToast.error({ 
-      color: 'red',
-      position: "topRight",          
-      message: `❌ Rejected promise in ${timer}ms`
-  })
-  });
-
+  promise
+    .then(value => {
+      iziToast.success({  
+        color: 'green',
+        position: "topRight",
+        message: `✅ Fulfilled promise in ${timer}ms`
+      })
+    })
+    .catch(error => {
+      iziToast.error({ 
+        color: 'red',
+        position: "topRight",          
+        message: `❌ Rejected promise in ${timer}ms`
+      })
+    });
 });
